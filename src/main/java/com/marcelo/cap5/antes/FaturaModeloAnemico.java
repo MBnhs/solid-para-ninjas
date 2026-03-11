e9bde98861bc7c02956efdd0d52cf1f4e2ab4262
MBnhs <marcelobanhoss@gmail.com>
2026-03-11 10:06:28 -0300

    Adicionando exemplo de modelo anemico do capítulo 5.

---
+++ src/main/java/com/marcelo/cap5/antes/FaturaModeloAnemico.java
@@ -0,0 +1,11 @@
+package com.marcelo.cap5.antes;
+
+import java.util.List;
+
+public class FaturaModeloAnemico {
+
+    private String cliente;
+    private double valor;
+    private List<Item> itens;
+
+}
